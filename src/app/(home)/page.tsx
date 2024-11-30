df0985cf79801349dd90dff106c47d8dd4733fe6
DDublue <theonly1living@gmail.com>
2024-11-30 02:46:39 -0800

    basic route groups

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+export default function HomePage() {
+
+  return (
+    <>
+      Hello
+      <button className="size-20 bg-blue-500 hover:bg-blue-600">
+        Clear redirect cookie
+      </button>
+    </>
+  )
+};
