fda153e5c0b53adcf5c4c360e7bb6f9442ba8ea8
DDublue <theonly1living@gmail.com>
2024-12-24 19:46:55 -0800

    Updated auth to work on edge runtime + conditional routing based on auth status

---
+++ auth.config.ts
@@ -0,0 +1,6 @@
+import Spotify from "next-auth/providers/spotify";
+import type { NextAuthConfig } from "next-auth";
+
+export default {
+  providers: [Spotify]
+} satisfies NextAuthConfig;
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,18 @@
 import NextAuth from "next-auth";
-import Spotify from "next-auth/providers/spotify";
- 
+import {DrizzleAdapter} from '@auth/drizzle-adapter';
+import { db } from "@/drizzle/db";
+import authConfig from "../../auth.config";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Spotify],
+  ...authConfig,
+  adapter: DrizzleAdapter(db),
+  session: {strategy: "jwt"},
   pages: {
     signIn: "/login",
   },
   callbacks: {
-    authorized: async ({ auth }) => {
-      return !!auth;
-    },
+    // authorized: async ({ auth }) => {
+    //   return !!auth;
+    // },
   },
 });
